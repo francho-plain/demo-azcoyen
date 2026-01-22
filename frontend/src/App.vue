94cbdfd47c5343a0c44453148c437aee939f440a
Francho Joven <fjoven@plainconcepts.com>
2026-01-22 19:00:48 +0100

    feat(frontend): us-5 crear proyecto vue 3.5+ con vuetify en /frontend [x]

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" dark>
+      <v-app-bar-title>Demo Azcoyen Copilot Tasks</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container class="mt-5">
+        <HelloWorld msg="¡Vuetify está funcionando!" />
+        <TheWelcome />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+/* Puedes personalizar estilos aquí si lo deseas */
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
